d960a14f5f4f2a152a3a20c3dd655fbfa30cc095
Emin Tolgahan Polat <emintolgahanpolat@gmail.com>
2024-01-02 14:08:27 +0300

    -

--- bricks/page/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
+++ bricks/page/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import '../../../core/base/base_widget.dart';
 import 'package:route_map/route_map.dart';
 import './{{name.snakeCase()}}_vm.dart';
 
 @RouteMap()
 class {{name.pascalCase()}}Page extends StatefulWidget {
-  const {{name.pascalCase()}}Page({ super.key }) : super(key: key);
+  
+  const {{name.pascalCase()}}Page({super.key});
   @override
   State<{{name.pascalCase()}}Page> createState() => _{{name.pascalCase()}}PageState();
 }
--- bricks/page/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_vm.dart
+++ bricks/page/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_vm.dart
@@ -1,3 +1,4 @@
+import '../../../core/base/base_view_model.dart';
 import 'package:injectable/injectable.dart';
 @injectable
 class {{name.pascalCase()}}ViewModel extends BaseViewModel {

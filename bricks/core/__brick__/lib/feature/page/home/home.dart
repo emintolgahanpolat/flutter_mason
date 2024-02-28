c450ad86044d7728ad67339303c3bfd5ec65449e
Emin Tolgahan Polat <emintolgahanpolat@gmail.com>
2024-02-28 11:06:59 +0300

    -

--- bricks/core/__brick__/lib/feature/page/home/home.dart
+++ bricks/core/__brick__/lib/feature/page/home/home.dart
@@ -1,6 +1,7 @@
 import '../../../core/base/base_widget.dart';
 import './home_vm.dart';
 import '../../../core/res/icons.dart';
+mport '../../../core/res/l10n/l10n.dart';
 import '../../router/app_router.routes.dart';
 import 'package:flutter/material.dart';
 import 'package:route_map/route_map.dart';
@@ -17,7 +18,7 @@ class _HomePageState extends BaseState<HomeViewModel, HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: const Text("Home"),
+      title: Text(context.l10n.home),
       actions: [
         IconButton.filled(
             onPressed: () {

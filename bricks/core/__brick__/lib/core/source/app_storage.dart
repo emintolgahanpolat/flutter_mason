8a9f9dcdbf717b09d31826d20bce0f34ce8b7d40
Emin Tolgahan Polat <emintolgahanpolat@gmail.com>
2024-07-26 13:08:12 +0300

    Update app_storage.dart

--- bricks/core/__brick__/lib/core/source/app_storage.dart
+++ bricks/core/__brick__/lib/core/source/app_storage.dart
@@ -5,7 +5,7 @@ class AppStorage {
   AppStorage(this._storage);
   final SharedPreferences _storage;
   T? read<T>(String key) {
-      String? readData = _storage.getString(AesEncrypt.encrypt(key));
+    String? readData = _storage.getString(AesEncrypt.encrypt(key));
     if (readData != null) {
       String data = AesEncrypt.decrypt(readData);
       switch (T) {

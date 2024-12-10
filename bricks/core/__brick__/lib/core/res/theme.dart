a27b631291fa17080c25926b9d4d98647fa63f96
Emin Tolgahan Polat <emintolgahanpolat@gmail.com>
2024-12-10 12:23:52 +0300

    -

--- bricks/core/__brick__/lib/core/res/theme.dart
+++ bricks/core/__brick__/lib/core/res/theme.dart
@@ -1,6 +1,7 @@
 import '../res/colors.gen.dart';
 import '../res/icons.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LightTheme extends AppTheme {
   @override
@@ -22,95 +23,47 @@ class AppTheme {
 
   ThemeData theme() {
     return ThemeData(
-        useMaterial3: true,
-        colorScheme: colorScheme,
-        appBarTheme: const AppBarTheme(
+      colorScheme: colorScheme,
+      fontFamily: GoogleFonts.poppins(fontWeight: FontWeight.w500).fontFamily,
+      dividerTheme: const DividerThemeData(space: 0),
+      appBarTheme: AppBarTheme(
           centerTitle: false,
           scrolledUnderElevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: colorScheme.onBrightness,
+            statusBarBrightness: colorScheme.brightness,
+            systemNavigationBarColor: Colors.transparent,
+            systemNavigationBarDividerColor: Colors.transparent,
+          )),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(
+          fontSize: 12,
         ),
-        dividerTheme: DividerThemeData(
-          space: 1,
-          color: colorScheme.onSurface.withOpacity(0.1),
+        unselectedLabelStyle: TextStyle(
+          fontSize: 12,
         ),
-        cardTheme: CardTheme(
-          clipBehavior: Clip.antiAlias,
-          shape: RoundedRectangleBorder(
-              borderRadius: const BorderRadius.all(Radius.circular(16)),
-              side: BorderSide(
-                  color: colorScheme.onSurface.withOpacity(0.8), width: 6)),
-        ),
-        textTheme:
-            const TextTheme(titleLarge: TextStyle(fontWeight: FontWeight.w700)),
-        listTileTheme: ListTileThemeData(
-          iconColor: colorScheme.onSurface.withOpacity(
-            0.5,
-          ),
-          titleTextStyle: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: colorScheme.onSurface,
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-        ),
-        actionIconTheme: ActionIconThemeData(
-          drawerButtonIconBuilder: (context) {
-            return const Icon(AppIcons.menu);
-          },
-          endDrawerButtonIconBuilder: (context) {
-            return const Icon(AppIcons.menu);
-          },
-          backButtonIconBuilder: (context) {
-            return const Icon(AppIcons.chevronLeft);
-          },
-          closeButtonIconBuilder: (context) {
-            return const Icon(AppIcons.xmark);
-          },
-        ),
-        iconTheme: IconThemeData(
-            color: colorScheme.onSurface.withOpacity(
-              0.5,
-            ),
-            size: 18),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            side: const BorderSide(color: AppColors.primarySwatch),
-          ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(
-                textStyle: const TextStyle(
-          fontWeight: FontWeight.bold,
-        ))),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: AppColors.primarySwatch,
-                foregroundColor: colorScheme.onPrimary,
-                textStyle: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                ))),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          type: BottomNavigationBarType.fixed,
-          selectedLabelStyle: TextStyle(fontSize: 12),
-          unselectedLabelStyle: TextStyle(
-            fontSize: 12,
-          ),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          isDense: true,
-          focusedBorder: inputBorder,
-          border: inputBorder,
-          filled: true,
-          alignLabelWithHint: true,
-          fillColor: colorScheme.onPrimary,
-          enabledBorder: inputBorder,
-          disabledBorder: inputBorder,
-        ));
+      ),
+      actionIconTheme: ActionIconThemeData(
+        drawerButtonIconBuilder: (context) {
+          return const Icon(AppIcons.menu);
+        },
+        endDrawerButtonIconBuilder: (context) {
+          return const Icon(AppIcons.menu);
+        },
+        backButtonIconBuilder: (context) {
+          return const Icon(AppIcons.chevronLeft);
+        },
+        closeButtonIconBuilder: (context) {
+          return const Icon(AppIcons.xmark);
+        },
+      ),
+    );
   }
+}
 
-  static var inputBorder = OutlineInputBorder(
-    gapPadding: 4,
-    borderSide: const BorderSide(color: AppColors.grey),
-    borderRadius: BorderRadius.circular(8),
-  );
+extension ColorsSchemeEx on ColorScheme {
+  Brightness get onBrightness =>
+      brightness == Brightness.light ? Brightness.dark : Brightness.light;
 }
